6d2bf5dffd69b26fd70868b886bdb5a365874f86
Jesus Morfin <jmorfin@csumb.edu>
2023-11-06 13:10:23 -0800

    More changes
    Almost finish with the inventory then the UI.

--- Assets/Scripts/Inventory/InventoryItem.cs
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -1,6 +1,6 @@
 using System;
 
-[Serializable]
+[System.Serializable]
 public class InventoryItem 
 {
     public InventoryItemData data { get; private set; }
--- Assets/Scripts/Inventory/ItemObject.cs
+++ Assets/Scripts/Inventory/ItemObject.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class ItemObject : MonoBehaviour
 {
     public InventoryItemData referenceItem;
-
     public void OnHandlePickupItem()
     {
         InventorySystem.current.Add(referenceItem);
