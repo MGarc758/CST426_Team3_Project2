8a98f86dc931e58793958ada4fa77def4a91ac22
JLaw1721 <jlawson@csumb.edu>
2023-11-08 08:42:40 -0800

    Finalized playtest vers of bloxor minigame
    It is still in a scuffed state but is still representative of the final concept

--- Assets/Scripts/BloxorScripts/RollerBehavior.cs
+++ Assets/Scripts/BloxorScripts/RollerBehavior.cs
@@ -1,122 +1,179 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class RollerBehavior : MonoBehaviour
 {
-    
-    public Transform pivotPoint;
-    public Transform block;
-    public LayerMask bloxorLevel;
-    public float rollTime = 1f; 
-    private bool inputActive;
+    public int rollSpeed = 250;
+    private bool moving = false;
+    public LayerMask detectionWalls;
+    public GameObject rotationPointObject;
 
-    private void Awake()
+    private void Update()
     {
-        inputActive = false;
-    }
-    
-    private void Roll()
-    {
-        StartCoroutine(RollToDirection);
-    }
-
-    private IEnumerator RollToDirection()
-    {
-        if (!inputActive)
+        if (moving == true)
         {
-            inputActive = true;
-            float angle = 90f;
-            
-            Vector3 axis = GetAxis();
-            Vector3 directionVector = GetDirectionVector();
-            Vector2 pivotOffset = GetPivotOffset();
+            return;
+        }
 
-            pivot.position = transform.position + (directionVector * pivotOffset.x) + (Vector3.down * pivotOffset.y);
-            
-            float elapsedTime = 0f;
-
-            while (elapsedTime < rollDuration)
-            {
-                elapsedTime += Time.deltaTime;
-
-                transform.RotateAround(pivot.position, axis, (angle * (Time.deltaTime / rollDuration)));
-                yield return null;
-            }
-
-            isRolling = false;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            StartCoroutine(RollRectangle(Vector3.right, Vector3.back));
+        } 
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            StartCoroutine(RollRectangle(Vector3.left, Vector3.forward));
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            StartCoroutine(RollRectangle(Vector3.forward, Vector3.right));
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            StartCoroutine(RollRectangle(Vector3.back, Vector3.left));
         }
         
     }
 
-
-    // private Vector3 GetAxis(Direction direction)
-    // {
-    //     switch (direction)
-    //     {
-    //         case Direction.Left:
-    //             return Vector3.forward;
-    //         case Direction.Up:
-    //             return Vector3.right;
-    //         case Direction.Right:
-    //             return Vector3.back;
-    //         case Direction.Down:
-    //             return Vector3.left;
-    //         default:
-    //             return Vector3.zero;
-    //     }
-    // }
-    //
-    // private Vector3 GetDirectionVector(Direction direction)
-    // {
-    //     switch (direction)
-    //     {
-    //         case Direction.Left:
-    //             return Vector3.left;
-    //         case Direction.Up:
-    //             return Vector3.forward;
-    //         case Direction.Right:
-    //             return Vector3.right;
-    //         case Direction.Down:
-    //             return Vector3.back;
-    //         default:
-    //             return Vector3.zero;
-    //     }
-    // }
-    //
-    // private Vector2 Offset(Direction direction)
-    // {
-    //     Vector2 offset = Vector2.zero;
-    //     
-    //     Vector2 center = transform.GetComponent<BoxCollider>().size / 2f;
-    //
-    //     RaycastHit hit;
-    //     if (Physics.Raycast(transform.position, transform.up, out hit, 100f, contactWallLayer))
-    //     {
-    //         switch (hit.collider.name)
-    //         {
-    //             case "X":
-    //                 if (direction == Direction.Left || direction == Direction.Right)
-    //                     pivotOffset = new Vector2(center.y, center.x);
-    //                 else
-    //                     offset = Vector2.one * center.x;
-    //                 break;
-    //             case "Y":
-    //                 pivotOffset = center;
-    //                 break;
-    //             case "Z":
-    //                 if (direction == Direction.Up || direction == Direction.Down)
-    //                     pivotOffset = new Vector2(center.y, center.x);
-    //                 else
-    //                     pivotOffset = Vector2.one * center.x;
-    //                 break;
-    //         }
-    //     }
-    //     return pivotOffset;
-    // }
-
-    public Vector3 getOffsetPoint()
+    IEnumerator RollRectangle(Vector3 rollDirection, Vector3 axisOfRotation)
     {
-        //Probably do something with raycasting, will require premade empty objects to serve as points
+        moving = true;
+        //Vector3 axisOfRotation = Vector3.Cross(Vector3.up, rollDirection);
+        Vector3 pivotPoint = Offset(rollDirection);
+
+        rotationPointObject.transform.position = pivotPoint;
+        transform.SetParent(rotationPointObject.transform);
+
+        float angleToBeRotated = 90f;
+
+        while (angleToBeRotated > 0)
+        {
+            float angleOfRotation = Mathf.Min(Time.deltaTime * rollSpeed, angleToBeRotated);
+            rotationPointObject.transform.RotateAround(pivotPoint, axisOfRotation, angleOfRotation);
+            angleToBeRotated -= angleOfRotation;
+            yield return null;
+        }
+        
+        rotationPointObject.transform.DetachChildren();
+        rotationPointObject.transform.localPosition = Vector3.zero;
+        rotationPointObject.transform.localEulerAngles = Vector3.zero;
+        moving = false;
+    }
+    
+    private Vector3 Offset(Vector3 dir)
+    {
+        Vector3 offset = Vector2.zero;
+        
+        Vector3 center = transform.position;
+    
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.up, out hit, 100f, detectionWalls))
+        {
+            switch (hit.collider.name)
+            {
+                case "X":
+                    // if (dir.Equals(Vector3.left) || dir.Equals(Vector3.right))
+                    //     offset = new Vector2(center.y, center.x);
+                    // else
+                    //     offset = Vector2.one * center.x;
+                    if (dir.Equals(Vector3.back))
+                    {
+                        offset = new Vector3(center.x, center.y - 0.5f, center.z - 0.5f);
+                    }
+
+                    if (dir.Equals(Vector3.forward))
+                    {
+                        offset = new Vector3(center.x, center.y - 0.5f, center.z + 0.5f);
+                    }
+                    
+                    if (dir.Equals(Vector3.left))
+                    {
+                        offset = new Vector3(center.x - 1f, center.y - 0.5f, center.z);
+                    }
+
+                    if (dir.Equals(Vector3.right))
+                    {
+                        offset = new Vector3(center.x + 1f, center.y - 0.5f, center.z);
+                    }
+                    break;
+                
+                case "Y":
+                    if (dir.Equals(Vector3.back))
+                    {
+                        offset = new Vector3(center.x, center.y - 1f, center.z - 0.5f);
+                    }
+
+                    if (dir.Equals(Vector3.forward))
+                    {
+                        offset = new Vector3(center.x, center.y - 1f, center.z + 0.5f);
+                    }
+                    
+                    if (dir.Equals(Vector3.left))
+                    {
+                        offset = new Vector3(center.x - 0.5f, center.y - 1f, center.z);
+                    }
+
+                    if (dir.Equals(Vector3.right))
+                    {
+                        offset = new Vector3(center.x + 0.5f, center.y - 1f, center.z);
+                    }
+                    break;
+                
+                case "Z":
+                    // if (dir.Equals(Vector3.forward) || dir.Equals(Vector3.back))
+                    //     offset = new Vector2(center.y, center.x);
+                    // else
+                    //     offset = Vector2.one * center.x;
+                    if (dir.Equals(Vector3.back))
+                    {
+                        offset = new Vector3(center.x, center.y - 0.5f, center.z - 1f);
+                    }
+
+                    if (dir.Equals(Vector3.forward))
+                    {
+                        offset = new Vector3(center.x, center.y - 0.5f, center.z + 1f);
+                    }
+                    
+                    if (dir.Equals(Vector3.left))
+                    {
+                        offset = new Vector3(center.x - 0.5f, center.y - 0.5f, center.z);
+                    }
+
+                    if (dir.Equals(Vector3.right))
+                    {
+                        offset = new Vector3(center.x + 0.5f, center.y - 0.5f, center.z);
+                    }
+                    break;
+            }
+        }
+        return offset;
+    }
+    //
+    // public Vector3 getOffsetPoint()
+    // {
+    //     //Probably do something with raycasting, will require premade empty objects to serve as points
+    // }
+
+    public bool CheckIfVertical()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.up, out hit, 100f, detectionWalls))
+        {
+            switch (hit.collider.name)
+            {
+                case "X":
+                    return false;
+
+                case "Y":
+                    return true;
+
+                case "Z":
+                    return false;
+            }
+        }
+
+        return false;
     }
 }
